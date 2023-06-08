4f6222bcc23bb0730709ae4a64a2574ea40a7e6d
Dennis Damien <dennis@livelocal.cloud>
2023-06-08 17:03:16 +0530

    index page added

--- src/main/java/com/tst/userregistrationlogin/web/MainController.java
+++ src/main/java/com/tst/userregistrationlogin/web/MainController.java
@@ -1,13 +1,25 @@
 package com.tst.userregistrationlogin.web;
 
+import com.tst.userregistrationlogin.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class MainController {
+
+    private UserService userService;
 
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+//        model.addAttribute("user",)
+        return "index";
     }
 }
